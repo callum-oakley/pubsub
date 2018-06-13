6d439fca43afe35b72fb979cc352c043482e174b
Callum Oakley <c.oakley108@gmail.com>
2018-06-13 20:09:21 +0100

    experimenting with channels

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,53 @@
 extern crate futures;
 extern crate hyper;
 
+use futures::sync::mpsc::{channel, Receiver, Sender};
 use futures::{future, Stream};
 use hyper::rt::{self, Future};
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::io::{Error, ErrorKind};
 use std::sync::{Arc, Mutex};
 
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
-struct Store {
-    stored: Mutex<Option<String>>,
+struct Bus {
+    sender: Mutex<Option<Sender<String>>>,
 }
 
-impl Store {
+impl Bus {
     fn new() -> Self {
-        Self {
-            stored: Mutex::new(None),
+        Bus {
+            sender: Mutex::new(None),
         }
     }
 
-    fn get(&self) -> Option<String> {
-        self.stored.lock().unwrap().clone()
+    fn subscribe(&self) -> Receiver<String> {
+        let (s, r) = channel(0);
+        *self.sender.lock().unwrap() = Some(s);
+        r
     }
 
-    fn set(&self, value: String) {
-        *self.stored.lock().unwrap() = Some(value);
+    fn send(&self, message: String) {
+        if let Some(mut s) = self.sender.lock().unwrap().clone() {
+            s.try_send(message).unwrap();
+        }
     }
 }
 
-fn handle(req: Request<Body>, store: Arc<Store>) -> BoxFut {
+fn handle(req: Request<Body>, bus: Arc<Bus>) -> BoxFut {
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
-            if let Some(stored) = store.get() {
-                *response.body_mut() = Body::from(stored);
-            } else {
-                *response.body_mut() = Body::from("nothing here yet!");
-            }
+            *response.body_mut() = Body::wrap_stream(
+                bus.subscribe()
+                    .map_err(|_| Error::new(ErrorKind::Other, "oh no!")),
+            )
         }
         (&Method::POST, "/") => {
             let response = req.into_body().concat2().map(move |data| {
-                store.set(String::from_utf8_lossy(&data.to_vec()).to_string());
+                bus.send(String::from_utf8_lossy(&data.to_vec()).to_string());
                 *response.status_mut() = StatusCode::CREATED;
                 response
             });
@@ -59,13 +64,13 @@ fn handle(req: Request<Body>, store: Arc<Store>) -> BoxFut {
 fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let store = Arc::new(Store::new());
+    let bus = Arc::new(Bus::new());
 
     let server = Server::bind(&addr)
         .serve(move || {
             // TODO there must be a nicer way of doing this
-            let store = Arc::clone(&store);
-            service_fn(move |req| handle(req, Arc::clone(&store)))
+            let bus = Arc::clone(&bus);
+            service_fn(move |req| handle(req, Arc::clone(&bus)))
         })
         .map_err(|e| eprintln!("server error: {}", e));
 
